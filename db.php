ecbbef9159b4b3d00d6738b01aa3b40d521e51e1
dwidyrtx <zhuruntaev.didar228@gmail.com>
2025-04-16 10:19:32 +0500

    Update db.php

--- db.php
+++ db.php
@@ -1,13 +1,9 @@
 <?php
 $servername = "localhost";
-$username = "root";  // Default XAMPP username
-$password = "";      // Default XAMPP password
+$username = "root";  
+$password = "";  
 $dbname = "todo_db";
-
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
